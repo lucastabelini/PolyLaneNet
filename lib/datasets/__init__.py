831c264c03b6b269c6fd28682f6a44718c25fabe
Lucas Tabelini Torres <lucastabelini@hotmail.com>
2020-04-22 00:43:42 -0300

    Remove unused file

--- lib/datasets/__init__.py
+++ lib/datasets/__init__.py
@@ -1,4 +1,4 @@
 from .lane_dataset import LaneDataset
 from .structured_points_dataset import StructuredPointsDataset
 
-__all__ = ["LaneDataset", "StructuredPointsDataset"]
+__all__ = ["LaneDataset"]
